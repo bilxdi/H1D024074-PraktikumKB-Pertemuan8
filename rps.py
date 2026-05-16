4c425293b7cb0695183b3f3b6c8dc5b5892c7116
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-05-16 16:14:23 +0700

    imagedatagenerator untuk data latih dan validasi

--- rps.py
+++ rps.py
@@ -5,3 +5,25 @@ from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 
+dataset_path = "./rockpaperscissors"
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_generator = train_datagen.flow_from_directory(
+    dataset_path,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training',
+)
+
+validation_generator = train_datagen.flow_from_directory(
+    dataset_path,
+    target_size=(150, 150),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation',
+)
