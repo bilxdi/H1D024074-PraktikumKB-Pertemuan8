49ff8a981eb8f58b46333323d0ca950d2ec32d7a
bilxdi <biladi.amna@mhs.unsoed.ac.id>
2026-05-16 16:12:06 +0700

    buat file main, import library

---
+++ rps.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
